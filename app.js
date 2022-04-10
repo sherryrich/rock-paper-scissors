a9551b01e737c1c05a4ba87cedca1918041bac4b
sherryrich <ricsherry@gmail.com>
2022-04-10 09:53:34 +0000

    random computer options working

--- app.js
+++ app.js
@@ -8,17 +8,35 @@ const game = () => {
         const introScreen = document.querySelector(".intro");
         const match = document.querySelector(".match");
 
-        playBtn.addEventListener("click", () => {
+        playBtn.addEventListener("click", () => { //shorthand function
             introScreen.classList.add("fadeOut");
             match.classList.add("fadeIn");
         });
     };
     //Play Match
+    const playMatch = () => {
+        const options = document.querySelectorAll(".options button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+        //Computer Options
+        const computerOptions = ["rock", "paper", "scissors"];
+        
+        options.forEach(option=>{
+            option.addEventListener("click", function() {
+                //Computer Choice
+              const computerNumber = Math.floor(Math.random() *3);
+              const computerChoice = computerOptions[computerNumber];
+              console.log(computerChoice)
+            });
+        });
 
+       
+    };
 
 
     //is call all the inner function
     startGame();
+    playMatch();
 };
 
 game();
